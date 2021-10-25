696dab88465bdb64d721485fe1726a54e0f32338
Userman <sam_deckstroyer@hotmail.com>
2021-10-25 10:51:37 +0200

    sitioPHP

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="js/prog.js"></script>
+    <LINK REL=StyleSheet HREF="css/css.css" TYPE="text/css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitios</title>
+
+</head>
+<body class="cuerpo">
+    
+
+<h1 id="hacheuno">Hola mundo tontito</h1>
+
+
+
+
+<?php
+echo "Hola mundo";
+?>
+
+
+</body>
+</html>
